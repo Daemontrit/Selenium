91f3de28daccb95cfa71e8fc84440df94573b019
Daemontrit <slavikk1999@yandex.ru>
2021-03-22 14:25:41 +0300

    1)удалены комментарии 2)созданы переменные для всех локаторов 3)хром дравйвер находится внутри проекта

--- src/main/java/Selenium.java
+++ src/main/java/Selenium.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Selenium {
     public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "D:\\ggez\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\IdeaProjects\\Selenium2\\src\\resources");
         WebDriver driver = new ChromeDriver();
         driver.get(" https://www.avito.ru/");
         System.out.println("захожу на сайт авито ");
@@ -22,7 +22,8 @@ public class Selenium {
         Select category = new Select(driver.findElement(By.id("category")));
         category.selectByValue("99");
         System.out.println("создаю элемент списка и выбираю нужную мне категорию в списке");
-        driver.findElement(By.id("search")).sendKeys("Принтер");
+        WebElement vvodPrinter =driver.findElement(By.id("search"));
+        vvodPrinter.sendKeys("Принтер");
         System.out.println("ввожу в поисковую строку 'Принтер'");
         WebElement clickGorod = driver.findElement(By.xpath("//div[@class='main-text-2PaZG']"));
         clickGorod.click();
@@ -32,7 +33,8 @@ public class Selenium {
         System.out.println("Ввожу нужный мне город");
         WebElement ckickFirstItem = driver.findElement(By.xpath("//li[@data-marker='suggest(0)']"));
         ckickFirstItem.click();
-        driver.findElement(By.xpath("//button[@data-marker='popup-location/save-button']")).click();
+        WebElement cliclPoisk= driver.findElement(By.xpath("//button[@data-marker='popup-location/save-button']"));
+        cliclPoisk.click();
         System.out.println("Выбираю и кликаю на первый элемент из списка, и кликаю по поиску");
         WebElement checkbox = driver.findElement(By.xpath("//div[contains(@data-marker,'delivery')]//label[contains(@class,'checkbox')]"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", checkbox);
@@ -40,7 +42,8 @@ public class Selenium {
             checkbox.click();
         }
         System.out.println("Проверяю активировано ли поле чекбокса и кликаю по нему");
-        driver.findElement(By.xpath("//button[@data-marker='search-filters/submit-button']")).click();
+        WebElement clickPoisk2=driver.findElement(By.xpath("//button[@data-marker='search-filters/submit-button']"));
+        clickPoisk2.click();
         System.out.println("кликаю по поиску");
         Select doroje=new Select(driver.findElement(By.xpath("//div[@class='sort-select-3QxXG select-select-box-3LBfK select-size-s-2gvAy']//select[@class='select-select-3CHiM']")));
         doroje.selectByValue("2");
@@ -59,5 +62,3 @@ public class Selenium {
 
     }
 }
-//span[@class='price-text-1HrJ_ text-text-1PdBw text-size-s-1PUdo']
-//h3[@class='title-root-395AQ iva-item-title-1Rmmj title-list-1IIB_ title-root_maxHeight-3obWc text-text-1PdBw text-size-s-1PUdo text-bold-3R9dt']/text(Полноцветный струйный принтер ComColor 3010)
